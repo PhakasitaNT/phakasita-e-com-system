fba8e6a04589fc12b357b6fba42c453263afc54a
Jinnawat Meecheepkij <jinnawat@hotmail.com>
2025-05-22 13:35:49 +0700

    PECS-16 trigger an email after successful registration completed

--- src/App.tsx
+++ src/App.tsx
@@ -17,6 +17,7 @@ function App() {
         </a>
       </div>
       <h1>E-com System 3.0.0 fixed bug (add login page)(update label cart button)(add grid ui)</h1>
+      <h1>(trigger an email after successfull registration)</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
